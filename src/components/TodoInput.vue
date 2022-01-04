4ebf3f36adfbf5bab370f69cc75174740aea680a
YeongKwonMo <momoci99@gmail.com>
2022-01-04 23:03:02 +0900

    chore:move dictory component

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,33 @@
+<template>
+	<input
+		v-model="todoText"
+		class="todo-input"
+		@input="update"
+		@keypress.enter="clear"
+	/>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+	data() {
+		return {
+			todoText: String,
+		};
+	},
+	methods: {
+		update(event: any) {
+			this.todoText = event.target.value;
+		},
+		clear() {
+			this.todoText = "";
+		},
+	},
+})
+export default class Input extends Vue {}
+</script>
+<style scoped lang="scss">
+.todo-input {
+}
+</style>
