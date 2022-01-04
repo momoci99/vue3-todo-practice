014566f8f3d348ba87bcf426b9af0d06333ede2a
YeongKwonMo <momoci99@gmail.com>
2022-01-04 23:14:41 +0900

    update:define vuex store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,34 @@
 import { createStore } from "vuex";
 
+// interface State {
+//   todoList: Array<string>;
+// }
+
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    todoList: [] as Array<string>,
+  },
+  mutations: {
+    addTodo(state, todo: string) {
+      state.todoList.push(todo);
+    },
+    clearTodo(state) {
+      state.todoList = [];
+    },
+    removeTodo(state, index: number) {
+      state.todoList.splice(index, 1);
+    },
+  },
+  actions: {
+    addTodo({ commit }, todo: string) {
+      commit("addTodo", todo);
+    },
+    clearTodo({ commit }) {
+      commit("clearTodo");
+    },
+    removeTodo({ commit }, index: number) {
+      commit("removeTodo", index);
+    },
+  },
   modules: {},
 });
