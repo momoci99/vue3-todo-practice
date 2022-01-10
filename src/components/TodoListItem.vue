0f4c29e39c9373643a27bdf6cb274c0036fda5ea
YeongKwonMo <momoci99@gmail.com>
2022-01-10 22:49:01 +0900

    feat:add delete button mock up

--- src/components/TodoListItem.vue
+++ src/components/TodoListItem.vue
@@ -1,5 +1,8 @@
 <template>
-	<article>{{ description }}</article>
+	<article class="todo-list-item">
+		<p class="item-description">{{ description }}</p>
+		<button class="item-delete">delete</button>
+	</article>
 </template>
 
 <script lang="ts">
@@ -11,6 +14,24 @@ import { Options, Vue } from "vue-class-component";
 	setup(props) {
 		console.log(props);
 	},
+	methods: {
+		delete: () => {
+			// this.$emit("delete");
+			console.log("delete");
+		},
+	},
 })
 export default class TodoListItem extends Vue {}
 </script>
+<style lang="scss">
+.todo-list-item {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 0.5rem;
+	border-bottom: 1px solid #ccc;
+
+	//shadow
+	box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
+}
+</style>
