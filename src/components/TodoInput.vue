7eba127e1c9d44adcd62199e16593509d0bce337
YeongKwonMo <momoci99@gmail.com>
2022-01-25 00:12:31 +0900

    style:input 태그 크기 그림자 변경

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -4,6 +4,7 @@
 		class="todo-input"
 		@input="update"
 		@keypress.enter="addTodoItem"
+		type="text"
 	/>
 </template>
 
@@ -34,5 +35,14 @@ export default class Input extends Vue {}
 </script>
 <style scoped lang="scss">
 .todo-input {
+	width: 400px;
+	height: 100px;
+	background-color: white;
+	box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+
+	padding: 0 30px;
+	font-size: 24px;
+
+	border-radius: 10px;
 }
 </style>
